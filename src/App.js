1d109a4c4cc0cf234bddb53b3ead128aea7fa093
Logan Pukarowski <l.pukarowski15@gmail.com>
2023-09-11 11:11:29 -0400

    set up components / added images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+const [bill, setBill] = useState(0);
+const [tip, setTip] = useState(0);
+const [group, setGroup] = useState(0);
+
   return (
     <div className="App">
-      
+      <h3>S P L I<br />T T E R</h3>
+      <div>
+        <div>
+          <label>Bill</label>
+          <input/>
+          <label>Select Tip %</label>
+          <input />
+          <label>Number of People</label>
+          <input />
+        </div>
+          <div>
+            <div>
+              <p>Tip Amount<span> / person</span></p>
+              <h4>{}</h4>
+            </div>
+            <div>
+              <p>Total<span> / person</span></p>
+              <h4>{}</h4>
+            </div>
+            <button>
+              <p>Reset</p>
+            </button>
+          </div>
+      </div>
     </div>
   );
 }
